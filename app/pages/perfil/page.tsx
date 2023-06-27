bdcfaa2c6ac25191ef404dac3a29cfd5921bdc63
João Paulo da Silveira <joao.silveira@g4f.com.br>
2023-06-27 13:53:45 -0300

    Navbar and pages

---
+++ app/pages/perfil/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: "About",
+}
+
+export default function Profile() {
+    return <div>Perfil</div>
+}
---
+++ app/pages/produtos/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: "About",
+}
+
+export default function Products() {
+    return <div>Produtos</div>
+}
